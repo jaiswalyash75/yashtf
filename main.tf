5e9445af43d68d3ad784e41428f386171ede3b42
EC2 Default User <ec2-user@ip-172-31-93-12.ec2.internal>
2020-09-10 10:12:09 +0000

    first commit

--- main.tf
+++ main.tf
@@ -8,24 +8,24 @@ module "vpc" {
 
 
 module "public" {
-  source = "./modules/public"
-  vpc_id = module.vpc.vpc_id
-  subnet_id= module.vpc.public_subnet_id
+  source    = "./modules/public"
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.vpc.public_subnet_id
 }
 
 
 module "rds" {
-  source = "./modules/rds"
-  vpc_id = module.vpc.vpc_id
-  subnet_ids= [module.vpc.private_subnet_id,module.vpc.rds_subnet_id]
-  private_sg_id= [module.private.private_sg_id]
+  source        = "./modules/rds"
+  vpc_id        = module.vpc.vpc_id
+  subnet_ids    = [module.vpc.private_subnet_id, module.vpc.rds_subnet_id]
+  private_sg_id = [module.private.private_sg_id]
 }
 
 
 module "private" {
-  source = "./modules/private"
-  vpc_id = module.vpc.vpc_id
-  subnet_id= module.vpc.private_subnet_id
-  address = module.rds.address
+  source    = "./modules/private"
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.vpc.private_subnet_id
+  address   = module.rds.address
 }
 
--- modules/private/output.tf
+++ modules/private/output.tf
@@ -1,3 +1,3 @@
 output "private_sg_id" {
- value = aws_security_group.server_private.id
+  value = aws_security_group.server_private.id
 }
